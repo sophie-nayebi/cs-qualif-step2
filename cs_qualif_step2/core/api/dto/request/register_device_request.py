6698938145567f90a415db0f8e67ddbac7cfa9df
Sophie Nayebi <sophie.nayebi.1@ulaval.ca>
2025-10-14 20:31:09 -0400

    Part 1 done

--- cs_qualif_step2/core/api/dto/request/register_device_request.py
+++ cs_qualif_step2/core/api/dto/request/register_device_request.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, validator
+import re
+import pytz
 
 class DeviceRegistrationRequest(BaseModel):
     macAddress: str
@@ -13,4 +15,22 @@ class DeviceRegistrationRequest(BaseModel):
     def not_empty(cls, v):
         if v is None or v.strip() == '':
             raise ValueError('Field cannot be empty or null')
+        return v
+
+    @validator('firmwareVersion')
+    def valid_firmware(cls, v):
+        if not bool(re.search("\d+\.\d+\.\d+", v)):
+            raise ValueError('Firmware version format is not valid')
+        return v
+
+    @validator('location')
+    def valid_location(cls, v):
+        if v is not None and not bool(re.search(".+, .+", v)):
+            raise ValueError('Location is not valid')
+        return v
+
+    @validator('timezone')
+    def valid_timezone(cls, v):
+        if v is not None and not v in pytz.all_timezones:
+            raise ValueError('Time zone is not valid')
         return v
